67c8bc84d2fbd594325b04282c873a6deabdf476
Alli Gilbreath <alligilbreath@dhcp-10-142-218-48.uawifi.arizona.edu>
2018-04-11 17:02:19 -0700

    Starting endToEnd test

--- src/testEndToEnd.cpp
+++ src/testEndToEnd.cpp
@@ -23,7 +23,23 @@ void EndToEndTester::RunTests(){
  * @requirement pass if all lines in files are the exact same (strcmp returns 0) AND output files can be opened properly
  * @return pass: 1, fail: 0
  */
-int EndToEndTester::TestFile(int testNum){    
+int EndToEndTester::TestFile(int testNum){
+    stringstream startHashtagsPath;
+    startHashtagsPath << TEST_FILE_RELATIVE_PATH << "/startHashtags" << testNum << ".txt";
+    
+    stringstream endHashtagsPath;
+    endHashtagsPath << TEST_FILE_RELATIVE_PATH << "/endHashtags" << testNum << ".txt";
+    
+    stringstream outputPath;
+    outputPath << TEST_FILE_RELATIVE_PATH << "/output" << testNum << ".txt";
+    
+    stringstream myOutputPath;
+    myOutputPath << TEST_FILE_RELATIVE_PATH << "/myOutput" << testNum << ".txt";
+    
+    
+    ifstream myOutputStream;
+    ifstream outputStream;
+    
   
     return false;
 }
